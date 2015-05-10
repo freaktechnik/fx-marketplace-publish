2534130c472fe3a5ccf56995e008f5a8f95f43d6
Martin Giger <martin.a.giger@sunrise.ch>
2015-05-10 21:22:11 +0200

    Initial additions

---
+++ README.md
@@ -0,0 +1,38 @@
+# fx-marketplace-publish
+Easily publish an application on the firefox marketplace
+
+## Install
+Install the package in your project using `npm install --save fx-marketplace-publish`.
+
+## Documentation
+### publish(options)
+`options`: Object
+
+The options object can have the following properties:
+
+  * `path`: Path to the webapp manifest in the web, or to a local packaged application
+  * `consumerKey`: Your consumer API key for the marketplace, inherited from [node-firefox-marketplace][], see their documentation for more info.
+  * `consumerSecret`: Your consumer API secret for the marketplace, inherited from [node-firefox-marketplace][], see their documentation for more info.
+  * `environment`: The marketplace you want to publish to, inherited from [node-firefox-marketplace][], see their documentation for more info. Defaults to `"development"`
+  * `type`: Optional webapp type specifier. Can be either `"manifest"` or `"packaged"`. Else the type is sniffed from the file extension in the `path` property.
+
+Returns a Promise, which resolves when the app is successfully published on the firefox marketplace, and gets rejected whenever an error occurs, including a failed validation.
+
+## Example
+This code snippet publishes a webapp packaged in `dist/my-webapp.zip` in the production evnironement of Firefox Marketplace (http://marketplace.firefox.com).
+```js
+var { publish } = require("fx-marketplace-publish");
+
+publish({
+  path: 'dist/my-webapp.zip',
+  consumerKey: "myConsumerKey",
+  consumerSecret: "myConsumerSecret",
+  environment: "production"
+});
+```
+
+## [License](LICENSE)
+This project is licensed under the MPL-2.0.
+
+[node-firefox-marketplace]: https://www.npmjs.com/package/node-firefox-marketplace
+
---
+++ index.js
@@ -0,0 +1,57 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+'use strict';
+
+var { Promise } = require('es6-promise');
+var MarketplaceClient = require("node-firefox-marketplace");
+
+var getTypeFromPath = function(path) {
+    if(path.match(/\.zip$/)) {
+        return "packaged";
+    }
+    else if(path.match(/\.webapp$/)) {
+        return "manifest";
+    }
+    else {
+        return "manifest":
+    }
+};
+
+/**
+ * Publish an open webapp on the firefox marketplace
+ * @param options: An options object with the required property 'path'. It
+ *                 inherits the properties of the construction parameters for
+ *                 node-firefox-marketplace. You can optionally specify the type
+ *                 of webapp you are publishing in the 'type' property, else it
+ *                 is sniffed using the file extension provided in the path and
+ *                 else defaults to "manifest".
+ * @return Promise, which resolves when the app is successfully published on
+ *         the firefox marketplace, and gets rejected whenever an error occurs,
+ *         including a failed validation.
+ */
+exports.publish = function(options) {
+    options.type = options.type || getTypeFromPath(options.path);
+    var client = new MarketplaceClient(options);
+
+    var promise = Promise(function(resolve, reject) {
+        var vaidator = client.validateManifest;
+        if(options.type == "packaged") {
+            validator = client.validatePackage;
+        }
+
+        validator(options.path).then(function(validation) {
+            if(validation.valid) {
+                client.publish(validation.id, options.type).then(resolve, reject);
+            }
+            else {
+                reject(validation.validation);
+            }
+        }, reject);
+    });
+    return promise;
+};
+
